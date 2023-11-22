2444d2a3660f43cec96fb2acd6bb63568fc18294
Jarno <jarno.marttila@sujuwa.fi>
2023-11-22 10:38:27 +0100

    testnet 4.0

--- src/io/custom-loader.js
+++ src/io/custom-loader.js
@@ -41,14 +41,63 @@ class CustomLoader extends BaseLoader {
     open(dataSource) {
         try {
             this._status = LoaderStatus.kConnecting;
-            let streamrClient = window.streamrClient = this._client = new StreamrClient({
+            let streamrClient = window.streamr = this._client = new StreamrClient({
+                logLevel: 'debug',
+                metrics: false,
                 auth: {
                   privateKey: "0x297882d5156658f9ba55d2269287c47d7c502557580fe1d2a55b82da25ee8272",
+                },
+                network: {
+                    controlLayer: {
+                        entryPoints: [
+                            {
+                                id: "e1",
+                                websocket: {
+                                    host: "entrypoint-1.streamr.network",
+                                    port: 40401,
+                                    tls: true
+                                }
+                            },
+                            {
+                                id: "e2",
+                                websocket: {
+                                    host: "entrypoint-2.streamr.network",
+                                    port: 40401,
+                                    tls: true
+                                }
+                            }
+                        ]
+                    }
+                },
+                contracts: {
+                    streamRegistryChainAddress: "0x4F0779292bd0aB33B9EBC1DBE8e0868f3940E3F2",
+                    streamStorageRegistryChainAddress: "0xA5a2298c9b48C08DaBF5D76727620d898FD2BEc1",
+                    storageNodeRegistryChainAddress: "0xE6D449A7Ef200C0e50418c56F84079B9fe625199",
+                    mainChainRPCs: {
+                        name: "mumbai",
+                        chainId: 80001,
+                        rpcs: [
+                            {
+                                url: "https://rpc-mumbai.maticvigil.com"
+                            }
+                        ]
+                    },
+                    streamRegistryChainRPCs: {
+                        name: "mumbai",
+                        chainId: 80001,
+                        rpcs: [
+                            {
+                                url: "https://rpc-mumbai.maticvigil.com"
+                            }
+                        ]
+                    },
+                    theGraphUrl: "https://api.thegraph.com/subgraphs/name/samt1803/network-subgraphs"
                 }
             })
             const streamId = dataSource.url
             console.log('streamId is: ', streamId)
-            streamrClient.subscribe('0x82a31ab84fd2159b54f887d4d8e46a0a1f3a7ffc/mapmetrics', (message) => {
+            streamrClient.subscribe('0x1518ae8bec297262abe1b81042a57f1d62add875/video-stream', (message) => {
+                //console.log(message)
                 let arrBuf = this.base64ToArrayBuffer(message['b'][1])
                 this._dispatchArrayBuffer(arrBuf)
             })
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         libraryTarget: 'umd'
     },
 
-    devtool: 'eval-source-map',
+    devtool: 'source-map',
 
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
