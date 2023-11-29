ca8af310ba1d002cfd43967656ec6f605d7670a3
Jarno <jarno.marttila@sujuwa.fi>
2023-11-29 20:30:30 +0100

    testnet update

--- src/io/custom-loader.js
+++ src/io/custom-loader.js
@@ -43,56 +43,9 @@ class CustomLoader extends BaseLoader {
             this._status = LoaderStatus.kConnecting;
             let streamrClient = window.streamr = this._client = new StreamrClient({
                 logLevel: 'debug',
-                metrics: false,
                 auth: {
                   privateKey: "0x297882d5156658f9ba55d2269287c47d7c502557580fe1d2a55b82da25ee8272",
                 },
-                network: {
-                    controlLayer: {
-                        entryPoints: [
-                            {
-                                id: "e1",
-                                websocket: {
-                                    host: "entrypoint-1.streamr.network",
-                                    port: 40401,
-                                    tls: true
-                                }
-                            },
-                            {
-                                id: "e2",
-                                websocket: {
-                                    host: "entrypoint-2.streamr.network",
-                                    port: 40401,
-                                    tls: true
-                                }
-                            }
-                        ]
-                    }
-                },
-                contracts: {
-                    streamRegistryChainAddress: "0x4F0779292bd0aB33B9EBC1DBE8e0868f3940E3F2",
-                    streamStorageRegistryChainAddress: "0xA5a2298c9b48C08DaBF5D76727620d898FD2BEc1",
-                    storageNodeRegistryChainAddress: "0xE6D449A7Ef200C0e50418c56F84079B9fe625199",
-                    mainChainRPCs: {
-                        name: "mumbai",
-                        chainId: 80001,
-                        rpcs: [
-                            {
-                                url: "https://rpc-mumbai.maticvigil.com"
-                            }
-                        ]
-                    },
-                    streamRegistryChainRPCs: {
-                        name: "mumbai",
-                        chainId: 80001,
-                        rpcs: [
-                            {
-                                url: "https://rpc-mumbai.maticvigil.com"
-                            }
-                        ]
-                    },
-                    theGraphUrl: "https://api.thegraph.com/subgraphs/name/samt1803/network-subgraphs"
-                }
             })
             //const streamId = dataSource.url
             //console.log('streamId is: ', streamId)
